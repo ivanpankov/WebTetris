a76f57fcac823e488c7de00b47945da570166aac
ivanpankov <ivanpankov@outlook.com>
2013-08-25 15:45:32 +0300

    wert

--- WebTetris/js/game-engine.js
+++ WebTetris/js/game-engine.js
@@ -1,46 +1,155 @@
-﻿// game object
-function GameObject(x, y, isAlive) {
-    this.x = x;
-    this.y = y;
-    this.isAlive = isAlive;
-}
-
-// shape
-function Rendable(x, y, width, height, isAlive) {
-    this.width = width;
-    this.height = height;
-
-    GameObject.call(this, x, y, isAlive);
-}
-Rendable.prototype = {
-    constructor: Rendable,
-    draw: function () {
-        console.log("Function draw() is not implemented!");
-    }
-}
-
-
-function Render(objArr, canvas) {
+﻿function Render(objArr, canvas) {
     var ctx = canvas.getContext('2d');
 
-    this.draw = function () {
-        var len = objArr.length,
-            i = 0;
+    function drawSquare(x, y, width, height, color) {
+        ctx.beginPath();
+        ctx.fillStyle = color;
+        ctx.lineWidth = "1px";
+        ctx.strokeStyle = "black";
+        ctx.rect(x, y, width, height);
+        ctx.fill();
+        ctx.stroke();
+    }
 
-        // clear canvas
+    function drawWalls() {
+
+    }
+
+    function drawShape(obj) {
+        var bodyArr = obj.getBody(),
+            lenY = bodyArr.length,
+            x;
+
+
+        for (var i = 0; i < lenY; i++) {
+            x = 2048;
+            for (var j = 0; j < 10; j++) {
+                x >>= 1;
+
+                if (bodyArr[i] & x) {
+                    drawSquare((obj.x + j * 24), (obj.y + i * 24), 22, 22, obj.color);
+                }
+            }
+        }
+    }
+
+    this.drawAll = function () {
+       // ctx.fillStyle = '#c69c6d';
+        //ctx.fillRect(23, 0, canvas.width - 48, canvas.height);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        //ctx.fill;
 
-        while (len--) {
-            objArr[i++].draw(ctx);
+        for (var i = 0; i < objArr.length; i++) {
+            drawShape(objArr[i]);
         }
     }
 }
 
 // the engine
-function GameEngine(canvas) {
+function GameEngine(playField) {
     var objectsQueue = [];
     var timeDelta;
-    var render = new Render(objectsQueue, canvas);
+    var render = new Render(objectsQueue, playField.canvas);
+    var theShape;
+
+    function getRandomShape() {
+        // get random number 1 - 7
+        var type = Math.floor((Math.random() * 7) + 1);
+        var shp;
+
+        switch (type) {
+            case 1:
+                shp = new shape('I');
+                break;
+
+            case 2:
+                shp = new shape('O');
+                break;
+
+            case 3:
+                shp = new shape('S');
+                break;
+
+            case 4:
+                shp = new shape('J');
+                break;
+
+            case 5:
+                shp = new shape('T');
+                break;
+
+            case 6:
+                shp = new shape('Z');
+                break;
+
+            case 7:
+                shp = new shape('L');
+                break;
+
+            default:
+                break;
+        }
+
+        return shp;
+    }
+
+    function isMovePossible(ghost) {
+        // check move possibility
+        for (var i = 0; i < ghost.length; i++) {
+            if (ghost[i] & playField.matrix[theShape.y + i]) {
+                return false; // move is impossible
+            }
+        }
+
+        // no overlap - the move is possible
+        return true;
+    }
+
+    function moveLeft() {
+        // clone shape body
+        var ghost = theShape.getGhost();
+        // shift cloning left
+        for (var i = 0; i < ghost.length; i++) {
+            ghost[i] <<= 1;
+        }
+
+        if (isMovePossible(ghost)) {
+            // move is possible
+            for (var i = 0; i < theShape.matrix.length; i++) {
+                for (j = 0; j < theShape.matrix[i].length; j++) {
+                    theShape.matrix[i][j] <<= 1;
+                }
+            }
+        }
+    }
+
+    function moveRight() {
+        // clone shape body
+        var ghost = theShape.getGhost();
+        // shift cloning right
+        for (var i = 0; i < ghost.length; i++) {
+            ghost[i] >>= 1;
+        }
+
+        if (isMovePossible(ghost)) {
+            // move is possible
+            for (var i = 0; i < theShape.matrix.length; i++) {
+                for (j = 0; j < theShape.matrix[i].length; j++) {
+                    theShape.matrix[i][j] >>= 1;
+                }
+            }
+        }
+    }
+
+    function rotate() {
+        var matrix = theShape.matrix;
+        var dir = theShape.direction + 1;
+        if (dir >= matrix.length) dir = 0;
+        var ghost = matrix[dir];
+        if (isMovePossible(ghost)) {
+            theShape.direction = dir;
+        }
+    }
 
     // time function
     var requestAnimFrame = (function (callback) {
@@ -54,29 +163,49 @@ function GameEngine(canvas) {
                 };
     })();
 
-    // anumate function
+    // animate function
     var animate = function (lastTime) {
         // update time
         var currentTime = new Date().getTime();
         timeDelta = currentTime - lastTime;
         lastTime = currentTime;
 
+
+        // keyboard
+        //if (Key.isDown(Key.UP)) this.moveUp();
+        if (Key.isDown(Key.LEFT) && !Key.done.left) {
+            Key.done.left = true;
+            moveLeft();
+        }
+        //if (Key.isDown(Key.DOWN)) this.moveDown();
+        if (Key.isDown(Key.RIGHT) && !Key.done.right) {
+            Key.done.right = true;
+            moveRight();
+        }
+        if (Key.isDown(Key.UP) && !Key.done.up) {
+            Key.done.up = true;
+            rotate();
+        }
+
         // update objects state
-        render.draw();
+        render.drawAll();
 
         // request new frame
-        //requestAnimFrame(function () {
-        //    animate(lastTime);
-        //});
+        requestAnimFrame(function () {
+            animate(lastTime);
+        });
     };
 
+    // start animation
     this.start = function () {
+        theShape = getRandomShape();
+        this.add(theShape);
+
         animate(new Date().getTime());
     };
 
     // add object to queue
     this.add = function (obj) {
-        obj.canvas = canvas;
         objectsQueue.push(obj);
     };
 }
---
+++ WebTetris/js/objects.js
@@ -0,0 +1,139 @@
+﻿
+var shapesColors = {
+    scheme1: {  // Vadim Gerasimov's Tetris 3.12
+        'I': '#AA0000',
+        'J': '#FFFFFF',
+        'L': '#AA00AA',
+        'O': '#0000AA',
+        'S': '#00AA00',
+        'T': '#AA5500',
+        'Z': '#00AAAA'
+    },
+    scheme2: {  // Microsoft Tetris
+        'I': '#FF0000',
+        'J': '#FF00FF',
+        'L': '#FFFF00',
+        'O': '#00FFFF',
+        'S': '#0000FF',
+        'T': '#C0C0C0',
+        'Z': '#00FF00'
+    },
+    scheme3: { // Sega/Arika (TGM series)
+        'I': '#FF0000',
+        'J': '#0000FF',
+        'L': '#FFA500',
+        'O': '#FFFF00',
+        'S': '#FF00FF',
+        'T': '#00FFFF',
+        'Z': '#00FF00'
+    },
+    scheme4: {  // The New Tetris and Kids Tetris
+        'I': '#00FFFF',
+        'J': '#8000FF',
+        'L': '#FF00FF',
+        'O': '#C0C0C0',
+        'S': '#00FF00',
+        'T': '#FFFF00',
+        'Z': '#FF0000'
+    },
+    scheme5: {  // The Tetris Company standardization (beginning with Tetris Worlds) Tetris Party
+        'I': '#00FFFF',
+        'J': '#0000FF',
+        'L': '#FFA500',
+        'O': '#FFFF00',
+        'S': '#00FF00',
+        'T': '#AA00FF',
+        'Z': '#FF0000'
+    },
+    scheme6: {  // Atari/Arcade
+        'I': '#FF0000',
+        'J': '#FFFF00',
+        'L': '#FF00FF',
+        'O': '#0000FF',
+        'S': '#00FFFF',
+        'T': '#00FF00',
+        'Z': '#FFA500'
+    },
+    scheme7: {  // TETЯIS The Soviet Mind Game
+        'I': '#FF0000',
+        'J': '#FFA500',
+        'L': '#FF00FF',
+        'O': '#0000FF',
+        'S': '#00FF00',
+        'T': '#808000',
+        'Z': '#00FFFF'
+    }
+};
+
+var colors = shapesColors.scheme1;
+
+function shape(type) {
+
+    this.direction = 0;
+    this.x = 1;
+    this.y = 1;
+    this.row = 0;
+    this.col = 3;
+    this.isAlive = true;
+    this.color;
+
+    switch (type) {
+        case "I":
+            this.matrix = [[120], [32, 32, 32, 32]];
+            this.color = colors.I;
+            break;
+
+        case "O":
+            this.matrix = [[48, 48]];
+            this.color = colors.O;
+            break;
+
+        case "S":
+            this.matrix = [[48, 96], [32, 48, 16]];
+            this.color = colors.S;
+            break;
+
+        case "J":
+            this.matrix = [[64, 120], [48, 32, 32, 32], [120, 8], [16, 16, 16, 48]];
+            this.color = colors.J;
+            break;
+
+        case "T":
+            this.matrix = [[32, 112], [32, 48, 32], [112, 32], [16, 48, 16]];
+            this.color = colors.T;
+            break;
+
+        case "Z":
+            this.matrix = [[48, 24], [16, 48, 32]];
+            this.color = colors.Z;
+            break;
+
+        case "L":
+            this.matrix = [[8, 120], [32, 32, 32, 48], [120, 64], [48, 16, 16, 16]];
+            this.color = colors.L;
+            break;
+
+        default:
+            throw new TypeError("The type of shape can be: I, O, S, J, T, Z or L !");
+            break;
+    }
+
+    
+}
+shape.prototype.getBody = function () {
+    if (this.direction >= this.matrix.length) {
+        this.direction = 0;
+    }
+    return this.matrix[this.direction];
+};
+shape.prototype.setBody = function (value) {
+    this.matrix[this.direction] = value;
+};
+shape.prototype.getGhost = function(){
+    var ghost = [];
+    var arr = this.getBody();
+    for (var i = 0; i < arr.length; i++) {
+        ghost.push(arr[i]);
+    }
+    return ghost;
+};
